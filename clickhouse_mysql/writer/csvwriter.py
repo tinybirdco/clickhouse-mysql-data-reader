74c532443dbcbfea4e9ec367a0cd2a204e45bc79
ygnuss <ygnuss@gmail.com>
2023-03-27 09:54:56 +0200

    Removed unsuported parameter newline

--- clickhouse_mysql/writer/csvwriter.py
+++ clickhouse_mysql/writer/csvwriter.py
@@ -253,7 +253,7 @@ class CSVWriter(Writer):
             if self.dst_table is None:
                 self.dst_table = event.table
 
-            self.writer = csv.DictWriter(self.file, fieldnames=self.fieldnames, quoting=csv.QUOTE_ALL, newline = '')
+            self.writer = csv.DictWriter(self.file, fieldnames=self.fieldnames, quoting=csv.QUOTE_ALL)
             if not self.header_written:
                 self.writer.writeheader()
 
